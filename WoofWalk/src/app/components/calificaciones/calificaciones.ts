9d09976a0a5ee705ced1be13104c28234b4c458f
Aldair Pariño <u202020356@upc.edu.pe>
2025-06-10 22:18:49 -0500

    Creacion Componente Calificaciones
    Creacion Componente Calificaciones

---
+++ WoofWalk/src/app/components/calificaciones/calificaciones.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calificaciones',
+  imports: [],
+  templateUrl: './calificaciones.html',
+  styleUrl: './calificaciones.css'
+})
+export class Calificaciones {
+
+}
