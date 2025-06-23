801a85082a8f3ef1fd5eba86a521d09e534f3c34
Aldair Pariño <u202020356@upc.edu.pe>
2025-06-22 19:49:02 -0500

    Creacion del componente MascotaEnfermedad
    Creacion del componente MascotaEnfermedad

---
+++ WoofWalk/src/app/components/mascotaenfermedad/listarmascenferm/listarmascenferm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarmascenferm',
+  standalone: true,
+  imports: [],
+  templateUrl: './listarmascenferm.component.html',
+  styleUrl: './listarmascenferm.component.css'
+})
+export class ListarmascenfermComponent {
+
+}
