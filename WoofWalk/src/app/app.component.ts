3b6abd65a899b3682b844d8c61df17edb995030b
Aldair Pariño <u202020356@upc.edu.pe>
2025-06-23 17:04:36 -0500

    agregacion de iconos al menu
    agregacion de iconos al menu

--- WoofWalk/src/app/app.component.ts
+++ WoofWalk/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { ClimaComponent } from "./components/clima/clima.component";
+import { LandingpageComponent } from "./components/landingpage/landingpage.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ClimaComponent],
+  imports: [ LandingpageComponent, MenuComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
